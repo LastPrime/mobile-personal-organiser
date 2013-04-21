9b04ebe41787bd6643aa4429a83f6734cd5c4d4f
Adei Josol <helena.josol@gmail.com>
2013-04-21 14:33:19 +0100

    Add task class

---
+++ Task.java
@@ -0,0 +1,31 @@
+public class Task {
+
+    private String name;
+    private String description;
+    private boolean done;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+}
